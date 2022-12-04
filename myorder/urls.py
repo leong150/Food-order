a7aeaf3450aa72ee03d0bee1fe0822de1646bb24
Leong Yong Sheng <108412901+leong150@users.noreply.github.com>
2022-12-04 13:04:10 +0800

    added user authentication

--- myorder/urls.py
+++ myorder/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .import views
 from OnlineMenu import settings
 from django.conf.urls.static import static
 from regex import P
+from django.contrib.auth import urls
 
 urlpatterns = [
     
@@ -11,5 +12,6 @@ urlpatterns = [
     path('new/add', views.add, name="add"),
     path('<menu_id>', views.food_detail, name="food_detail"),
     path('<menu_id>/edit', views.edit, name="edit"),
+    path('accounts/', include('django.contrib.auth.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myorder/views.py
+++ myorder/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import menu, menuform
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import views as auth_views
 
 # Create your views here.
 
@@ -12,10 +15,12 @@ def home(request):
     context = {'latest_menu':latest_menu}
     return render(request, 'myorder/home_page.html', context)
 
+@login_required(login_url='accounts/login/')
 def food_detail(request, menu_id):
     current_menu = menu.objects.get(pk=menu_id)
     return render(request, 'myorder/edit_food.html', {'current_menu':current_menu})
 
+@login_required(login_url='accounts/login/')
 def edit(request, menu_id):
     latest_menu = menu.objects.get(pk=menu_id)
     form = menuform(request.POST, request.FILES, instance=latest_menu)
@@ -30,9 +35,11 @@ def edit(request, menu_id):
         print("error")
         return HttpResponseRedirect(reverse('food_detail', args=menu_id))
 
+@login_required(login_url='accounts/login/')
 def new_food_detail(request):
     return render(request, 'myorder/add_food.html')
 
+@login_required(login_url='accounts/login/')
 def add(request):
     form = menuform(request.POST, request.FILES)
     if form.is_valid():
@@ -42,3 +49,4 @@ def add(request):
     else:
         print("error")
         return render(request, 'myorder/add_food.html')
+
